6423fb506198188891fca036d5452eb0bcb39cc4
JohanFlotten <johanflotten@gmail.com>
2024-02-09 21:49:29 +0100

    buh

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("randomCatBtn").addEventListener("click", function() {
-    var catImages = ["/images/frickuashley.jpg", "/images/lilla søt.jpg", "/images/PogOcat.jpg", "/images/lilla.png", "/lillab.jpg"]; // Array of cat image URLs
+    var catImages = ["../images/frickuashley.jpg", "../images/lilla søt.jpg", "../images/PogOcat.jpg", "../images/lilla.png", "/lillab.jpg"];
     var randomIndex = Math.floor(Math.random() * catImages.length); // Generate a random index
     var randomCatImage = catImages[randomIndex]; // Get a random cat image URL
     document.getElementById("catImage").src = randomCatImage; // Set the src attribute of the cat image
