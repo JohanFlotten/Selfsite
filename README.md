0cb5787aa97863dc053eb7af6cefdc061b30c2b6
JohanFlotten <johanflotten@gmail.com>
2024-02-09 17:57:02 +0100

    fikset alt som oppgaven spurte etter. laget også en enkel script i js som viser ett tilfeldig bilde av katten min.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Prosjekt uke 1
 har laget en helt mega basic side med litt info om meg selv.
+
+laget en flersidet nettside med litt info om meg selv, en side der jeg tenker å legge till spill jeg blir å lage i fremtiden. har også laget en basic script i js som viser ett tilfeldig bilde av katten min hver gang man trykker på en knapp.
---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("randomCatBtn").addEventListener("click", function() {
+    var catImages = ["/images/frickuashley.jpg", "/images/lilla søt.jpg", "/images/PogOcat.jpg", "/images/lilla.png", "/lillab.jpg"]; // Array of cat image URLs
+    var randomIndex = Math.floor(Math.random() * catImages.length); // Generate a random index
+    var randomCatImage = catImages[randomIndex]; // Get a random cat image URL
+    document.getElementById("catImage").src = randomCatImage; // Set the src attribute of the cat image
+});
